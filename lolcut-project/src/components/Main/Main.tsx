db1823ff6a143599b67921c8c0bf4de6c58fac9e
이성진 <chlskatnr1@naver.com>
2020-05-16 14:38:33 +0900

    [ADD] <Main> 메인 기본 틀 작성
    2020.05.16

---
+++ lolcut-project/src/components/Main/Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as Styled from './Styled';
+
+interface Props {
+
+}
+
+const Main: React.FC<Props> = () => {
+    return (
+        <Styled.Main>
+            
+        </Styled.Main>
+    )
+}
+
+export default Main;
