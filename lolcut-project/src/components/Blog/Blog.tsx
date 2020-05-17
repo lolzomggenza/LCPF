28eb0db27e0757bc5e3444ad6b5d0f999c9a21e4
asdf <migsking@naver.com>
2020-05-18 05:01:09 +0900

    [MODIFY] <src/components/Blog/*> 카드 위치 조정

--- lolcut-project/src/components/Blog/Blog.tsx
+++ lolcut-project/src/components/Blog/Blog.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from 'react';
 import * as Styled from './Styled';
-import { Header } from '../../components';
+import { Header, Card } from '../../components';
 
 
 const Blog: React.FC = () => {
@@ -9,13 +9,57 @@ const Blog: React.FC = () => {
         <Styled.BlogMain>
             <Header />
             <main>
-                <div>
-
+                <div className="post-wrapper">
+                    <div>
+                        <Card />
+                    </div>
+                    <div>
+                        <Card />
+                    </div>
+                    <div>
+                        <Card />
+                    </div>
+                    <div>
+                        <Card />
+                    </div>
+                    <div>
+                        <Card />
+                    </div>
+                    <div>
+                        <Card />
+                    </div>
+                    <div>
+                        <Card />
+                    </div>
+                    <div>
+                        <Card />
+                    </div>
+                    <div>
+                        <Card />
+                    </div>
+                    <div>
+                        <Card />
+                    </div>
+                    <div>
+                        <Card />
+                    </div>
                 </div>
-            </main>
-            <aside>
+                <aside>
+                    <div className="notice-wrapper">
+                        <span>
+                            hello
+                        </span>
+                        <ul>
+                            <li>공지 1</li>
+                            <li>공지 2</li>
+                            <li>온라인 수업 시러욧</li>
+                        </ul>
+                    </div>
+                    <div className="tag-wrapper">
 
-            </aside>
+                    </div>
+                </aside>
+            </main>
         </Styled.BlogMain>
     )
 }
--- lolcut-project/src/components/Blog/Styled.tsx
+++ lolcut-project/src/components/Blog/Styled.tsx
@@ -2,11 +2,24 @@ import styled from "styled-components";
 
 export const BlogMain = styled.div`
     > main {
-        background-color: #f8f9fa;
+        display: flex;
         width: 100%;
-        height: 2000px;
-    }
-    > aside {
+        height: 100%;
+        background-color: #f8f9fa;
 
+        .post-wrapper {
+            padding: 5% 3% 10% 5%;
+            display: flex;
+            width: 72%;
+            flex-wrap: wrap;
+            > div {
+                padding: 0 0.81% 3%; 0;
+            }
+        }
+        > aside {
+            width: 20%;
+            height: 900px;
+            background-color: red;
+        }
     }
 `;
