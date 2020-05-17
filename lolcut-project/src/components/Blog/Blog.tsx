fdd33e0b2889ed9ef6ad4d4355ec35cd5bbb7610
asdf <migsking@naver.com>
2020-05-18 04:08:54 +0900

    [ADD] <src/components/Blog/*> Blog 메인 관련 파일 추가

---
+++ lolcut-project/src/components/Blog/Blog.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useCallback } from 'react';
+import * as Styled from './Styled';
+import { Header } from '../../components';
+
+
+const Blog: React.FC = () => {
+
+    return (
+        <Styled.BlogMain>
+            <Header />
+            <main>
+                <div>
+
+                </div>
+            </main>
+            <aside>
+
+            </aside>
+        </Styled.BlogMain>
+    )
+}
+
+export default Blog
---
+++ lolcut-project/src/components/Blog/Styled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const BlogMain = styled.div`
+    > main {
+        background-color: #f8f9fa;
+        width: 100%;
+        height: 2000px;
+    }
+    > aside {
+
+    }
+`;
