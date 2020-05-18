a832c3891c3a66900c76fd04d949dad229cc1878
asdf <migsking@naver.com>
2020-05-18 09:13:36 +0900

    [ADD] <src/App.tsx> blog route 추가

--- lolcut-project/src/components/index.js
+++ lolcut-project/src/components/index.js
@@ -1,9 +1,11 @@
 import Header from './Header/Header.tsx';
 import Main from './Main/Main.tsx';
-import Card from './Card/Card.tsx'
+import Card from './Card/Card.tsx';
+import Blog from './Blog/Blog.tsx'
 
 export {
     Header,
     Main,
-    Card
+    Card,
+    Blog
 }
